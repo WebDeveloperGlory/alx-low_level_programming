b7e19a68345e1a221e3e5a6cf4256da29ca457fd
Glory <gloryumuerri223@gmail.com>
2022-12-12 16:49:32 -0800

    modifications

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-	  printf("With proper grammar, the outcome is a piece of art.\n");
+	  printf("With proper grammar, but zthe outcome is a piece of art,\n");
 	    return 0;
 }
 
