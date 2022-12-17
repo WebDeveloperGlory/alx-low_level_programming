5584d2937ce0d96f50cada7812968ab955a96a6e
Glory <gloryumuerri223@gmail.com>
2022-12-16 19:49:23 -0800

    super func

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * _isupper - check if a character is an uppercase letter
@@ -11,6 +11,25 @@ int _isupper(int c)
 	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
+	}
+	return (0);
+}
+
+/**
+ * main - check if a character is an uppercase letter
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char c = 'A';
+	if (_isupper(c))
+	{
+		printf("%c is an uppercase letter\n", c);
+	}
+	else
+	{
+		printf("%c is not an uppercase letter\n", c);
 	}
 	return (0);
 }
