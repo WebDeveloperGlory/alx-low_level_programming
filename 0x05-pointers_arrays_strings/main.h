8e8f32ca28fe17dca1f09229d9134ffc99667aba
Glory <gloryumuerri223@gmail.com>
2022-12-19 17:11:21 -0800

    lets see

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,14 +1,3 @@
 #include <stdio.h>
 
-void reset_to_98(int *ptr)
-{
-	*ptr = 98;
-}
-void swap_int(int *a, int *b)
-{
-	int tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
+void reset_to_98(int *ptr);
