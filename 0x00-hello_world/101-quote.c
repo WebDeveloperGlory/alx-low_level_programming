d9c06e45a187bf8c53ae0aac83222bca69a8fc23
Glory <gloryumuerri223@gmail.com>
2022-12-20 23:17:15 -0800

    even more, more corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
+	char msg[100] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+
 	write(STDERR_FILENO, msg, sizeof(msg));
 	return (1);
 }
