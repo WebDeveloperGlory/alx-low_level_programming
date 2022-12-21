9bf894d931d0abe9283f7ba9c921e1c24cf312fa
Glory <gloryumuerri223@gmail.com>
2022-12-20 22:30:53 -0800

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 1.
+ */
+int main(void)
+{
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, msg, sizeof(msg));
+	return (1);
+}
+
