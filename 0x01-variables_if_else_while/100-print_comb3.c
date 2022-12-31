e95a95553d3327a6802e07973021f0b78c348df5
Glory <gloryumuerri223@gmail.com>
2022-12-31 07:21:31 -0800

    genius

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - output some numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i, j;
+	
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if ((i == 8) && (j == 9))
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
