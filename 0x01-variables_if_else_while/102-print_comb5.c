563092798fd57d1a70b2c13b3729f395567f92f7
Glory <gloryumuerri223@gmail.com>
2022-12-31 08:00:52 -0800

    finally

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - print some numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if ((i / 10 == 9) && (i % 10 == 8) && (j / 10 == 9) && (j % 10 == 9))
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n'); 
+	return (0);
+}
