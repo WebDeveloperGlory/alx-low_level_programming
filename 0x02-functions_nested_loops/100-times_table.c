ed4c87b0a80c39665d97159012187e010de81811
Glory <gloryumuerri223@gmail.com>
2023-01-02 00:35:37 -0800

    hello

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_times_table - prints the nth time table
+ * @n: interger
+ */
+void print_times_table(int n)
+{
+	int i, j;
+
+	if (!(n > 15) && !(n < 0))
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				_putchar('0' + i * j);
+				_putchar(',');
+				_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+}
