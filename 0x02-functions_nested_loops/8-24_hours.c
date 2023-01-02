20866e75e956f58743ca014228c989618b0ae457
Glory <gloryumuerri223@gmail.com>
2023-01-01 23:36:21 -0800

    24hrs huh

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#incluse "main.h"
+/**
+ * jack_bauer - prints every minuite
+ */
+void jack_bauer(void)
+{
+	char mins, hrs;
+
+	for (hrs = 0; hrs < 24; hrs++)
+	{
+		if (hrs < 10)
+			_putchar('0');
+		
+		_putchar(hrs + '0');
+		_putchar(':');
+		for (mins = 0; mins < 60; mins++)
+		{
+			if (mins < 10)
+				_putchar('0');
+
+			_putchar(mins + '0');
+			if (hrs != 23 && mins != 59)
+				_putchar('\n');
+		}
+	}
+}
+
+
