62428ffede5e8ec5faf8359c38b28675e9424717
Glory <gloryumuerri223@gmail.com>
2023-01-02 04:15:18 -0800

    heo

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 /**
- * fibonacci_sum - Calculates the sum of the even-valued terms in the Fibonacci sequence whose values do not exceed a given maximum value.
+ * fibonacci_sum - Calculates the sum of the even-valued terms 
+ * in the Fibonacci sequence whose values do not exceed 
+ * a given maximum value.
+ *
  * @max_value: The maximum value of the terms to include in the sum.
  * Return: The sum of the even-valued terms in the Fibonacci sequence
 */
@@ -24,7 +27,10 @@ int fibonacci_sum(int max_value)
 	return (sum);
 }
 /**
- * main - Tests the `fibonacci_sum` function by printing the sum of the even-valued Fibonacci terms whose values do not exceed 4,000,000.
+ * main - Tests the `fibonacci_sum` function by 
+ * printing the sum of the even-valued Fibonacci terms 
+ * whose values do not exceed 4,000,000.
+ * 
  * Return: always 0
  */
 int main(void)
