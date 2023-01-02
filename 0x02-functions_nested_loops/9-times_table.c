c5b5171aa58fd5cce883db22e9df635ba6d0d8fd
Glory <gloryumuerri223@gmail.com>
2023-01-02 02:19:56 -0800

    hello

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * times_table - prints 9 times table
+ */
+void times_table(void)
+{
+	int i, j, rst;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			rst = i * j;
+			if (rst < 10)
+				_putchar(' ');
+			else
+				_putchar((rst / 10) + '0');
+			
+			_putchar((rst % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
