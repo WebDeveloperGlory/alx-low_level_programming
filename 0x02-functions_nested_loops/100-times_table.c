eae948e5ef57e8abc4a42eaae6503b2c8849dfae
Glory <gloryumuerri223@gmail.com>
2023-01-02 01:04:51 -0800

    hopefully

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,14 +14,20 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				int rst = i * j;
+
 				if (rst < 10)
 				{
 					_putchar('0' + rst);
 				}
+				else if (rst > 99)
+				{
+					_putchar('0' + (rst / 100));
+					_putchar('0' + (rst / 10));
+					_putchar('0' + (rst % 10));
 				else
 				{
-					_putchar('0' + rst / 10);
-					_putchar('0' + rst % 10);
+					_putchar('0' + (rst / 10));
+					_putchar('0' + (rst % 10));
 				}
 				if (j != n)
 				{
